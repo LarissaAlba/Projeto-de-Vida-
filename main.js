d4901f539d4f1b4ec4cceaa329b22f39eb3b31f7
Larissa Alba <alba.santos.larissa@escola.pr.gov.br>
2026-05-13 15:35:12 -0300

    lamax2026

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const botoes=document.querySelectorAll(".botao");
+const textos=document.querySelectorAll(".aba-conteudo");
 console.log(botoes);
+
 
 for (let i=0; i<= botoes.length; i++){
     botoes[i].onclick=function{
 
         for (let i=0;i;j<botoes.length;j++){
           botoes[j].classList.remove("ativo");  
+          textos[j].classList.remove("ativo"); 
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 console.log(botoes[i]);
 }
